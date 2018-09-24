da2324587f71249e8b0480464293f81f237d6c58
Elurajnagare <36191192+Elurajnagare@users.noreply.github.com>
2018-09-24 11:04:57 +0530

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,27 @@
+<?php include "includes/header.php" ?>
+<section id="blog-slider" class="slider page">
+  <div class="intro-container">
+    <div id="introCarousel" class="carousel d-block slide carousel-fade" data-ride="carousel">
+      <ol class="carousel-indicators d-none">
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="d-flex carousel-item active" style="background-image:url('images/slider-img1.png'); background-position:right bottom; background-size:contain; ">
+          <div class="carousel-container">
+            <div class="container carousel-content">
+              <h2>Blog</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<main id="main">
+	<div id="blog" class="blog">
+     
+    	</div>
+	<div class="clearfix"></div>
+	<?php include "includes/testimonials.php" ?>
+	<div class="clearfix"></div>
+</main>
+<?php include "includes/footer.php" ?>
